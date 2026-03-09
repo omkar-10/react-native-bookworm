b0f08889d4bbaed009f558ff6ebe8d6d6474cef3
Omkar Dhavale <dhavale.omkar10@gmail.com>
2026-03-09 18:27:00 +0530

    increase body limit to 10mb

--- src/server.js
+++ src/server.js
@@ -9,7 +9,7 @@ import job from "./lib/cron.js";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 
 app.use("/api/auth", authRoutes);
