63900c7b3ff3fcd16b995dbb6f343202efbead43
Omkar Dhavale <dhavale.omkar10@gmail.com>
2026-03-09 18:36:28 +0530

    user created at fix

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -62,6 +62,7 @@ router.post("/register", async (req, res) => {
         username: user.username,
         email: user.email,
         profileImage: user.profileImage,
+        createdAt: user.createdAt,
       },
     });
   } catch (error) {
@@ -104,6 +105,7 @@ router.post("/login", async (req, res) => {
         username: user.username,
         email: user.email,
         profileImage: user.profileImage,
+        createdAt: user.createdAt,
       },
     });
   } catch (error) {
